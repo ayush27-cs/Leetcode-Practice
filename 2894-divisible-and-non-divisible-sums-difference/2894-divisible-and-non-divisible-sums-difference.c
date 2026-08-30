19754bde3108fb1304226b4f4621aaf87f6a7f7d
Ayush Singh Thakur <singhthakurayush176@gmail.com>
2026-08-30 19:00:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.7%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.c
@@ -0,0 +1,13 @@
+int differenceOfSums(int n, int m) {
+    int sum=0;
+    for(int i =0;i<=n;i++){
+        if(i%m!=0){
+            sum+=i;
+        }
+        else{
+            sum-=i;
+        }
+    }
+    return sum;
+    
+}
